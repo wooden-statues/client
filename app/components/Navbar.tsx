869155043db2248876059cc484301e9858627b82
dean10042008 <dean.yurukov2008@gmail.com>
2025-06-18 14:57:27 +0300

    changed to next Image tag

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
     return (
         <nav className="flex justify-between items-center py-9 px-50 bg-dark-green">
-            <picture>
-                <img src="/logo.svg" alt="logo" />
-            </picture>
+            <Image src="/logo.svg" alt="logo" width={116} height={41} />
 
             <ul className="flex gap-15 font-lemonada text-xl text-[#F4ECE2] font-[575]">
                 <li><Link href="/">Начало</Link></li>
